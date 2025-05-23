82e30068b9a94cb703d342b438ffd7b7ddccecd7
duniapineda <duniaacosta62@gmail.com>
2025-05-23 12:52:37 -0600

    Add files via upload

---
+++ c#29/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Proyecto_C_29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imglFotos = new System.Windows.Forms.ImageList(this.components);
+            this.lblFoto = new System.Windows.Forms.Label();
+            this.btnCambio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imglFotos
+            // 
+            this.imglFotos.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imglFotos.ImageStream")));
+            this.imglFotos.TransparentColor = System.Drawing.Color.Transparent;
+            this.imglFotos.Images.SetKeyName(0, "4.jpg");
+            this.imglFotos.Images.SetKeyName(1, "5.jpg");
+            this.imglFotos.Images.SetKeyName(2, "6.jpg");
+            this.imglFotos.Images.SetKeyName(3, "7.jpg");
+            this.imglFotos.Images.SetKeyName(4, "8.jpg");
+            this.imglFotos.Images.SetKeyName(5, "9.jpg");
+            this.imglFotos.Images.SetKeyName(6, "10.jpg");
+            this.imglFotos.Images.SetKeyName(7, "vv.jpg");
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.ImageIndex = 0;
+            this.lblFoto.ImageList = this.imglFotos;
+            this.lblFoto.Location = new System.Drawing.Point(86, 9);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(373, 232);
+            this.lblFoto.TabIndex = 0;
+            // 
+            // btnCambio
+            // 
+            this.btnCambio.Location = new System.Drawing.Point(89, 237);
+            this.btnCambio.Name = "btnCambio";
+            this.btnCambio.Size = new System.Drawing.Size(173, 39);
+            this.btnCambio.TabIndex = 1;
+            this.btnCambio.Text = "Cambiar Imagen";
+            this.btnCambio.UseVisualStyleBackColor = true;
+            this.btnCambio.Click += new System.EventHandler(this.btnCambio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCambio);
+            this.Controls.Add(this.lblFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imglFotos;
+        private System.Windows.Forms.Label lblFoto;
+        private System.Windows.Forms.Button btnCambio;
+    }
+}
+
---
+++ c#29/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_C_29
+{
+    public partial class Form1: Form
+    {
+        private int indice;
+        public Form1()
+        {
+            InitializeComponent();
+            indice = 0;
+        }
+
+        private void btnCambio_Click(object sender, EventArgs e)
+        {
+            indice++;
+
+            if (indice > 7)
+                indice = 0;
+
+            lblFoto.ImageIndex = indice;
+        }
+    }
+}
